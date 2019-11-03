46975f9dfed26ef0c62302a96a7c2a79df8874b0
TrackRunny <trackrunny@protonmail.com>
2019-11-02 18:36:48 -0700

    Update README, Add Source Code

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# PyCrossClicker
-A console autoclicker that is made with Python 3. 
+<!-- MAIN TITLE -->
+# • PyCrossClicker
+
+<!-- PICTURE -->
+<img align="right" src="http://i.imgur.com/GjG9eSW.png" width=38%>
+
+<!-- BADGES -->
+![Python Versions](https://img.shields.io/badge/python-3.5%20%7C%203.6%20%7C%203.7-blue?style=flat-square)
+![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg?style=flat-square)
+
+<!-- KEY INFORMATION HEADER -->
+## → Details / Features
+
+* Fully Functional Autoclicker
+* Console Application
+* Custom Keyboard Shortcuts
+* Customize Clicker Speed
+* Simple, Easy, Fast to use
+
+<!-- INSTALLATION HEADER -->
+## → Installation Instructions
+
+* You can run the provided exe or binary application for your operating system, or you may install the dependences needed and run this application with python.
+
+```markdown
+pip install pynput
+<!-- Remember: Only Use this if you only have python 3 installed. -->
+
+or
+
+pip3 instlal pynput
+<!-- Remember: Use this if you have two versions of python and / or you have python 2 and python 3. -->
+```
+
+Next clone the repo and change directories into the project.
+
+```markdown
+git clone https://github.com/TrackRunny/PyCrossClicker.git
+
+cd PyCrossClicker
+```
+
+Then run the file!
+
+```markdown
+python pycrossclicker.py
+<!-- Remember: Only Use this if you only have python 3 installed. -->
+
+or
+
+python3 pycrossclicker.py
+<!-- Remember: Use this if you have two versions of python and / or you have python 2 and python 3. -->
+```
+
+<!-- LICENSE INFO -->
+## → License
+
+* This project is licensed under the GPLv3.
---
+++ pycrossclicker.py
@@ -0,0 +1,126 @@
+import time
+import threading
+from pynput.mouse import Button, Controller
+from pynput.keyboard import Listener, KeyCode
+import os
+
+divider = "————————————————————————————————————————————————————————"
+
+try:
+    clicks = float(input("Delay speed: (Default: 0.060 Seconds ~ 15 Clicks Per Second): "))
+    delay = clicks
+except ValueError:
+    clicks = 0.060
+    delay = clicks
+
+print(divider)
+
+# ———————————————————————————————————————————————————————
+# ———————————————————————————————————————————————————————
+# ———————————————————————————————————————————————————————
+
+input_start_and_stop_key = str(input("Start And Stop Key: (Default: x): "))
+start_stop_key = KeyCode(char=input_start_and_stop_key)
+
+if input_start_and_stop_key == "":
+    start_stop_key = KeyCode(char="x")
+
+print(divider)
+
+# ———————————————————————————————————————————————————————
+# ———————————————————————————————————————————————————————
+# ———————————————————————————————————————————————————————
+
+input_exit_key = str(input("Exit Key: (Default: `): "))
+exit_key = KeyCode(char=input_exit_key)
+
+if input_exit_key == "":
+    exit_key = KeyCode(char="`")
+
+print(divider)
+
+# ———————————————————————————————————————————————————————
+# ———————————————————————————————————————————————————————
+# ———————————————————————————————————————————————————————
+
+input_button = str(input("Options: Left, Right, Middle"
+                         "\nMouse button: (Default: Left Mouse Button): "))
+input_button = input_button.lower()
+button = Button.left
+
+if input_button == "right":
+    button = Button.right
+elif input_button == "middle":
+    button = Button.middle
+else:
+    pass
+
+print(divider)
+
+# ———————————————————————————————————————————————————————
+# ———————————————————————————————————————————————————————
+# ———————————————————————————————————————————————————————
+
+
+class ClickMouse(threading.Thread):
+    def __init__(self, delay, button):
+        super(ClickMouse, self).__init__()
+        self.delay = delay
+        self.button = button
+        self.running = False
+        self.program_running = True
+
+    def start_clicking(self):
+        self.running = True
+
+    def stop_clicking(self):
+        self.running = False
+
+    def exit(self):
+        self.stop_clicking()
+        self.program_running = False
+
+    def run(self):
+        while self.program_running:
+            while self.running:
+                mouse.click(self.button)
+                time.sleep(self.delay)
+            time.sleep(0.1)
+
+
+# ———————————————————————————————————————————————————————
+# ———————————————————————————————————————————————————————
+# ———————————————————————————————————————————————————————
+
+
+mouse = Controller()
+click_thread = ClickMouse(delay, button)
+click_thread.start()
+
+
+print("Clicker is ready!")
+
+# ———————————————————————————————————————————————————————
+# ———————————————————————————————————————————————————————
+# ———————————————————————————————————————————————————————
+
+
+def on_press(key):
+    if key == start_stop_key:
+        if click_thread.running:
+            click_thread.stop_clicking()
+            os.system("clear")
+            print("Clicking as stopped!")
+            print(divider)
+        else:
+            click_thread.start_clicking()
+            os.system("clear")
+            print("Clicking as started!")
+            print(divider)
+    elif key == exit_key:
+        click_thread.exit()
+        listener.stop()
+
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
